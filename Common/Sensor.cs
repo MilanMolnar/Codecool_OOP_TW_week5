ab51572cb4ad52b0901b1f1996371760c89af998
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-03-03 13:22:53 +0100

    init

---
+++ Common/Sensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    class Sensor
+    {
+        public string ID { get; set; }
+        public Measurement MeasurementOfSensor { get; set; }
+
+        public void AddMeasurement()
+        {
+
+        }
+    }
+}
---
+++ Common/XMLHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class XMLHandler
+    {
+        public void SavetoXml()
+        {
+
+        }
+        public Dictionary<string,Measurement> LoadFromXml() //String will be the sensor's ID
+        {
+            return null;
+        }
+    }
+}
