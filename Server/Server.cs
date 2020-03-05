e6211178a2e8fecfc39eff45439eead8f4323166
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-03-05 14:29:38 +0100

    a

--- Server/Server.cs
+++ Server/Server.cs
@@ -13,7 +13,21 @@ namespace Server
 
         public static void Main()
         {
-            
+
+            //var date = 1583407660898;
+            //var datenow = DateTime.Now.Ticks;
+            //var dateresult = datenow - date;
+            //DateTime dateDate = new DateTime(dateresult);
+            //var date2 = 637190113074752355;
+            //DateTime dateDate2 = new DateTime(date2);
+            //DateTime now = DateTime.Now;
+            //var result = now - (dateDate);
+
+            //Console.WriteLine(dateDate.ToString("yyyy'-'MM'-'dd'  'HH':'mm':'ss"));
+            //Console.WriteLine(dateDate2.ToString("yyyy'-'MM'-'dd'  'HH':'mm':'ss"));
+            //Console.ReadKey();
+
+
             //var date2 = DateTime.Now.Ticks;
             //DateTime date3 = new DateTime(date2);
             //Console.WriteLine(date3.ToString("yyyy'-'MM'-'dd'  'HH':'mm':'ss"));
@@ -24,7 +38,7 @@ namespace Server
             //Console.WriteLine(DateTime.Now.Millisecond);
             //Console.ReadKey();
             //192.168.150.15
-            IPAddress ipAddress = IPAddress.Parse("192.168.150.222"); // google: how to get ipv4 in c#, elso talalat stackoverflow es nem lesz hardcodeolva. Just a tipp :)
+            IPAddress ipAddress = IPAddress.Parse("192.168.150.222"); 
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 12345);
 
             Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
@@ -37,7 +51,9 @@ namespace Server
                 allDone.Reset();
 
                 Console.WriteLine("Waiting for a connection...");
+                
                 listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+                
 
                 allDone.WaitOne();
             }
@@ -46,36 +62,43 @@ namespace Server
 
         private static void AcceptCallback(IAsyncResult ar)
         {
-            allDone.Set();
+            try
+            {
+                allDone.Set();
 
-            // Get the socket that handles the client request.  
-            Socket listener = (Socket)ar.AsyncState;
-            Socket client = listener.EndAccept(ar);
 
-            //WHILE loop
-            
-            byte[] buff = new byte[1024];
-            int bytesReads = client.Receive(buff);
-            
+                Socket listener = (Socket)ar.AsyncState;
+                Socket client = listener.EndAccept(ar);
+
+
+                byte[] buff = new byte[1024];
+                int bytesReads = client.Receive(buff);
+
                 if (bytesReads < buff.Length)
                 {
                     using (System.IO.StreamWriter file =
                     new System.IO.StreamWriter("recieved.xml", false))
                     {
-
                         file.WriteLine(Encoding.ASCII.GetString(buff, 0, bytesReads));
                         Console.WriteLine("[INFO] Got data {0} ", bytesReads);
                     }
-                    
+
                 }
                 else
                 {
                     Console.WriteLine("KEX");
 
                 }
-            
-            client.Close();
+
+                client.Close();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("exception");
+            }
 
         }
+
     }
 }
