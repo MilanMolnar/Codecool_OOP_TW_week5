7b2cb150d569dd443c1c12a205762352221760a5
jakabij <jakabistvanj@gmail.com>
2020-03-05 10:58:45 +0100

    ClientMenu completed

--- Client/ClientMenu.cs
+++ Client/ClientMenu.cs
@@ -9,34 +9,92 @@ namespace Client
 {
     public class ClientMenu
     {
-        public void AskForIntervall()
+        public void Modify(Client client, Sensor sensor)
         {
+            while(true)
+            {
+                Console.WriteLine("Press:\n\t- 1) to set Server IP\n\t- 2) to set your ID.\n");
+                string input = Console.ReadLine();
 
+                if (input.Equals("1"))
+                {
+
+                }
+                else if (input.Equals("2"))
+                {
+                    while(true)
+                    { 
+                        Console.WriteLine("Press:\n\t- 1) to set Server IP\n\t- 2) to set your ID.\n\t" +
+                        " -3) to set Type of measurement.\n\t- 4) to go back for main menu.");
+                        if (input.Equals("1"))
+                        {
+                            Console.Write("IP to connect: ");
+                            string ip = Console.ReadLine();
+                            string[] tmp=ip.Split(".");
+                            if(tmp.Length!=4)
+                            {
+                                throw new Exception("NotValidIP");
+                            }
+
+
+                            try
+                            {
+                                foreach(var item in tmp)
+                                {
+                                    byte.Parse(item);
+                                }
+                            }
+                            catch
+                            {
+                                throw new Exception("NotValidAttributes");
+                            }
+
+                            client.ServerIPAddress = new byte[] { byte.Parse(tmp[0]),
+                                byte.Parse(tmp[1]), byte.Parse(tmp[2]), byte.Parse(tmp[3]) };
+                        }
+
+                        else if (input.Equals("2"))
+                        {
+                            input = Console.ReadLine();
+                            sensor.ID = input;
+                        }
+                        else if (input.Equals("3"))
+                        {
+                            Console.WriteLine("\n\tPress:\n\t\t- 1) for celsius.\n\t\t" +
+                                "- 2) for humidity.\n\t\t- 3) for air pressure.");
+                            input = Console.ReadLine();
+                            Random random = new Random();
+                            if (input.Equals("1"))
+                            {
+                                sensor.MeasurementOfSensor.MeasurementType = "Celsius";
+                                sensor.MeasurementOfSensor.Value = random.Next(-20, 40).ToString();
+                            }
+                            else if (input.Equals("2"))
+                            {
+                                sensor.MeasurementOfSensor.MeasurementType = "Humidity";
+                                sensor.MeasurementOfSensor.Value = random.Next(0, 90).ToString();
+                            }
+                            else if (input.Equals("3"))
+                            {
+                                sensor.MeasurementOfSensor.MeasurementType = "Air pressure";
+                                sensor.MeasurementOfSensor.Value = random.Next(0, 3000).ToString();
+                            }
+                            else
+                                throw new Exception("InvalidInput");
+                        }
+                        else if (input.Equals("4"))
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                    throw new Exception("InvalidInput!");
+
+                
+            }
         }
-
-        public void StartClient()
-        {
-            //Console.Write("IP to connect: ");
-            //string ip = Console.ReadLine();
-            //string[] tmp=ip.Split(".");
-            //if(tmp.Length!=4)
-            //{
-            //    throw new Exception("NotValidIP");
-            //}
-
-
-            //try
-            //{
-            //    foreach(var item in tmp)
-            //    {
-            //        byte.Parse(item);
-            //    }
-            //}
-            //catch
-            //{
-            //    throw new Exception("NotValidAttributes");
-            //}
-        }
+       
         public static void Main()
         {
             Client client = new Client();
